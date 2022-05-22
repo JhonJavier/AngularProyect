1d5eb80480955470992c15dae835db6198e2e12e
Jhon Rivera <88907513+JhonJavier@users.noreply.github.com>
2022-05-22 11:18:58 -0500

    nuevos cambios

--- src/app/modalidad/modalidad.service.ts
+++ src/app/modalidad/modalidad.service.ts
@@ -22,7 +22,7 @@ export class ModalidadService {
     return this.http.post<Modalidad>(this.url, modalidad);
   }
 
-  //Obtener modalidad por id
+  //Obtener modalidad por un id
   get(id_modalidad:number):Observable<Modalidad>{
     return this.http.get<Modalidad>(this.url+'/'+id_modalidad);
   }
